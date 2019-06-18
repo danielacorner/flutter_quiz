2d14fb00fa7f9ee0a5fb86ed20805f37b377e227
dcorn068 <dcorn068@users.noreply.github.com>
2019-06-17 21:53:16 -0400

    extract AnswerButton & QuestionText modules

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:quiz/pages/quiz_page.dart';
 
 import './pages/landing_page.dart';
 
 void main() {
   runApp(new MaterialApp(
-    home: new LandingPage(),
+    home: new QuizPage(),
   ));
 }
